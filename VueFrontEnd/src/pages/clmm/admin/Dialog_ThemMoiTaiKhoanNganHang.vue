64e1ed9a30d2d795a4335d9f6f3f32c8551b234e
TrungTran2751999 <trantrung2751999@gmail.com>
2024-11-03 22:05:06 +0700

    newww

---
+++ VueFrontEnd/src/pages/clmm/admin/Dialog_ThemMoiTaiKhoanNganHang.vue
@@ -0,0 +1,37 @@
+<template>
+    <v-dialog
+      v-model="dialog"
+    >
+    <v-btn>ahihi</v-btn>
+    </v-dialog>
+</template>
+<script>
+  export default {
+    data () {
+      return {
+        dialog: this.setDialog
+      }
+    },
+    props:{
+      setDialog:{
+        type: Boolean,
+      }
+    },
+    methods:{
+      setValueDialog(){
+        
+      }
+    },
+    watch:{
+      setDialog(){
+         // tuong trung cho useEffect trong Reactjs
+      }
+    },
+    components:{
+      
+    }
+  }
+</script>
+<style scope>
+
+</style>
